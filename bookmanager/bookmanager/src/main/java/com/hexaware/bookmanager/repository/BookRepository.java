adaeff3fc24cb6113462cfe3fd91b57b5aaa160b
Monish Coumar <116776241+mc-monish28@users.noreply.github.com>
2025-06-27 17:52:32 +0530

    Delete bookmanager/bookmanager directory

--- bookmanager/bookmanager/src/main/java/com/hexaware/bookmanager/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hexaware.bookmanager.repository;
-
-import com.hexaware.bookmanager.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-} 
